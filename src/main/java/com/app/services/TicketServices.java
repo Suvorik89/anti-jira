9c3735865beb4462d9a8c7fd8a5b4ab5488b4a25
Jekaterina Suvorova <kateupgo@gmail.com>
2024-03-19 20:28:26 +0200

    Anti Jira - Project + Ticket

--- src/main/java/com/app/services/TicketServices.java
+++ src/main/java/com/app/services/TicketServices.java
@@ -2,6 +2,7 @@ package com.app.services;
 
 import com.app.dao.TicketsDAO;
 import com.app.model.Ticket;
+import com.app.model.User;
 import com.app.security.CustomUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -19,11 +20,19 @@ public class TicketServices {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         CustomUserDetails customUser = (CustomUserDetails) authentication.getPrincipal();
 
-        ticket.setReporterId(customUser.getUser().getId());
+        User user = new User();
+        user.setId(customUser.getUser().getId());
+        //ticket.setReporterId(customUser.getUser().getId());
+
+        ticket.setUser(user);
         ticketsDAO.storeNewTicket(ticket);
     }
 
     public List<Ticket> getTicketsByProject(long projectId) {
         return ticketsDAO.getTicketsByProject(projectId);
+    }
+
+    public List<Ticket> getLastTickets(int count) {
+        return ticketsDAO.getLastTickets(count);
     }
 }
